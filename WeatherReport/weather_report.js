2b10c0af1eca157f989bf6d71185e07ab815479d
Jorge <millanjorge@gmail.com>
2025-01-07 19:39:50 +0100

    Add weather report practice addons

--- WeatherReport/weather_report.js
+++ WeatherReport/weather_report.js
@@ -9,8 +9,12 @@ function showWeatherDetails(event) {
         .then(data => {
             const weatherInfo = document.getElementById('weatherInfo');
             weatherInfo.innerHTML = `<h2>Wheather in ${data.name}</h2>
-            <p>Temperature: ${data.main.temp}°C</p>
-            <p>Weather: ${data.weather[0].description}</p>`;
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather icon">
+            <p>Temperatura: ${data.main.temp}°C</p>
+            <p>Sensación térmica: ${data.main.feels_like}°C</p>
+            <p>Humedad: ${data.main.humidity}%</p>
+            <p>Viento: ${data.wind.speed * 3600 / 1000} Km/h</p>
+            <p>Tiempo: ${data.weather[0].description}</p>`;
         })
         .catch(error => {
             console.error('Error fetching weather:', error);
